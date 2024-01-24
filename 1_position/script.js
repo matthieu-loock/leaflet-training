12a0939ef537628b2d2142c583d0c81d3ed134a3
matthieu-loock <m01ck@proton.me>
2024-01-24 23:19:21 +0100

    Simple affichage

--- 1_position/script.js
+++ 1_position/script.js
@@ -1,24 +1,24 @@
 /* eslint-disable no-undef */
 /**
- * Simple map
+ * Simple carte
  */
 
-// config map
+// config de la map
 let config = {
   minZoom: 7,
   maxZoom: 18,
 };
-// magnification with which the map will start
+// zoom de départ
 const zoom = 18;
-// co-ordinates
-const lat = 52.22977;
-const lng = 21.01178;
+// coordonnées de Fréchencourt
+const lat = 49.9652;
+const lng = 2.4415;
 
-// calling map
+// appel de la map
 const map = L.map("map", config).setView([lat, lng], zoom);
 
-// Used to load and display tile layers on the map
-// Most tile servers require attribution, which you can set under `Layer`
+// utiliser pour afficher les tuiles de la map
+// la plupart des serveurs de tuiles demandent une source que l'on peut ajouter après "attribution"
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
