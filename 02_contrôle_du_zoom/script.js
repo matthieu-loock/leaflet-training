7d8fa445927dbbcdf6e86cadd2e8d76a7bfe1174
matthieu-loock <m01ck@proton.me>
2024-02-12 19:35:51 +0100

    Traduction des commentaires

--- 02_contrôle_du_zoom/script.js
+++ 02_contrôle_du_zoom/script.js
@@ -7,24 +7,24 @@
 let config = {
   minZoom: 7,
   maxZoom: 18,
-  zoomControl: false, // Zoom on
+  zoomControl: false, // Zoom on/off
 };
 // Grossisement de départ
 const zoom = 18;
-// co-ordinates
+// coordonées
 const lat = 49.9652;
 const lng = 2.4415;
 
-// calling map
+// Appel de la map
 const map = L.map("map", config).setView([lat, lng], zoom);
 
-// Used to load and display tile layers on the map
-// Most tile servers require attribution, which you can set under `Layer`
+// utiliser pour afficher les tuiles de la map
+// la plupart des serveurs de tuiles demandent une source que l'on peut ajouter après "attribution"
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-// reactivate zoom at the desired location
+// Contrôle du zoom
 // [topleft, topright, bottomleft, bottomright]
 L.control.zoom({ position: "bottomright" }).addTo(map);
